805f7e71443f880e43ffce18b1e7ccac00e8aa39
Paul Pessoa <paulmspessoa@gmail.com>
2023-05-30 06:29:26 -0300

    importing componentss and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+// layout
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+// components
+import NotificationBar from './components/NotificationBar';
+import Catalog from './components/Catalog';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NotificationBar/>
+      <Header />
+      <Catalog />
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
