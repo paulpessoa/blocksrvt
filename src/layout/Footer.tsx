a2aae548ac10e7780aa77b00282d140904d00d82
Paul Pessoa <paulmspessoa@gmail.com>
2023-05-30 06:11:15 -0300

    footer component

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <a href="/#" className="link">
+          Sobre
+        </a>
+        <a href="/#" className="link">
+          FAQ
+        </a>
+        <a href="/#" className="link">
+          Termos de Uso
+        </a>
+        <a href="/#" className="link">
+          Política de Privacidade
+        </a>
+    </footer>
+  );
+};
+
+export default Footer;
